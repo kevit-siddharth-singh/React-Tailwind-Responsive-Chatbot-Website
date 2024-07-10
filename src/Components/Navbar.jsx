37ed53afec187f2e2b590e1b781022ed99c5b77a
kevit-siddharth-singh <siddharth.singh@kevit.io>
2024-07-10 16:11:45 +0530

    Added intial commit

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,21 @@
+import logo from "../assets/bot.png";
+
+function Navbar() {
+  return (
+    <nav className="navbar w-100 flex justify-around px-20 py-5">
+      <div className="logo w-12">
+        <img src={logo} alt="logo of the company" />
+      </div>
+      <div className="links  w-1/3 flex justify-between items-center px-10 text-white font-[700]">
+        <a href="">Resources</a>
+        <a href="">Pricing</a>
+        <button className="text-[16px] text-[#7EA6FF] px-5 py-2 border-[#7EA6FF] border-2 rounded-[0.375rem] cursor-pointer">
+          Sign in
+        </button>
+        <button className="text-[16px] bg-[#4B83FF] px-5 py-2 rounded-[0.375rem]">Create a skybot</button>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
