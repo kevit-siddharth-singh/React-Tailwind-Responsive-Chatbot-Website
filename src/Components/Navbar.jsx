5c11cc616b600b502071be361041b8c298c5c20b
kevit-siddharth-singh <siddharth.singh@kevit.io>
2024-07-11 17:11:52 +0530

    Added Style

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,10 +6,10 @@ function Navbar() {
       <div className="logo w-[8.5rem] ">
         <img className="logo-img " src={logo} alt="logo of the company" />
       </div>
-      <div className="links  w-1/3 flex justify-between items-center px-10 text-white font-[700]">
+      <div className="links  w-1/3 flex justify-between items-center px-1 text-white font-[700]">
         <a href="">Resources</a>
         <a href="">Pricing</a>
-        <button className="text-[16px] text-[#7EA6FF] px-5 py-2 border-[#7EA6FF] border-2 rounded-[0.375rem] cursor-pointer">
+        <button className="sign-in-btn text-[16px] text-[#7EA6FF] px-5 py-2 border-[#7EA6FF] border-2 rounded-[0.375rem] cursor-pointer">
           Sign in
         </button>
         <button className="glow-on-hover text-[16px] bg-[#1A5FFF] px-5 py-2 rounded-[0.375rem] ">
